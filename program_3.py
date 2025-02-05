53ecf251b147ecc35e0289f0d5fcdfc6e8ddfc82
ParkerPrograms <njparker2@students.unwsp.edu>
2025-02-04 20:23:03 -0600

    Add files via upload

--- program_3.py
+++ program_3.py
@@ -1,32 +1,19 @@
-# Programming Excersize 3-13
+#Nathan Parker
+#2/4/25
+#Program #3: Shipping Charges
 
-# The Fast Freight Shipping Company charges the following rates:
+#get the weight of the item from the user
+weight = float(input('Enter the weight of the item in pounds: '))
 
-# Weight    	Price Per Pound
-# 2 pounds or less   	$1.50
-# Over 2 pounds but not more than 6 pounds  	$3.00
-# Over 6 pounds but not more than 10 pounds	$4.00
-# Over 10 pounds	$4.75
-# Write a program which calculates the shipping charge and displays the total.
+#determine into which catagory the weight falls
+if weight <= 2:
+    price = weight * 1.50
+elif weight > 2 and weight <= 6:
+    price = weight * 3.00
+elif weight > 6 and weight <= 10:
+    price = weight * 4.00
+else:
+    price = weight * 4.75
 
-def weight_conversion(weight):
-    # Calculate the shipping charge.
-    shippingCost = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
-    
-    return shippingCost
-
-#### This piece of the code has been done for you,
-#### you only need to worry about the actual shipping 
-#### charge logic in the weight_conversion function
-if __name__ == '__main__':
-    # Local variables
-    weight = 0.0
-    shippingCost = 0.0
-    # Get package weight from the user.
-    weight = float(input('Enter the weight of the package: '))
-    # Display the shipping charge.
-    shippingCost = weight_conversion(weight)
-    print ('Shipping charge: $', format(shippingCost, '.2f'))
+#display the shipping price
+print(f'The shipping price is ${price:,.2f}.')
--- program_5.py
+++ program_5.py
@@ -1,6 +1,61 @@
-# There are two kinds of hot dogs sold:  
-# Hot Dog ($3.50), Chili Dog ($4.50).  
-# Additionally a person can order cheese ($0.50), peppers ($0.75), or grilled onions ($1.00).  
-# Also tax is 7%. 
-# Write a program the inputs the type of hot dog wanted and optional toppings.  
-# The program then displays the hot dog cost, tax and total cost. 
+#Nathan Parker
+#2/4/25
+#Program #5: Hot Dog
+
+#define variables
+cheese = ''
+peppers = ''
+grilled_onions = ''
+
+#get the type of hot dog that the user wants
+hot_dog_type = input('Would you like a "hot dog" ($3.50) or a "chili dog" ($4.50)?: ')
+
+#if the user wants a hot dog, note the price and ask what toppings the user wants
+if hot_dog_type == 'hot dog':
+    hot_dog_price = 3.50
+    cheese = input('Would you like cheese ($0.50) on your hot dog? (yes or no): ')
+    peppers = input('Would you like peppers ($0.75) on your hot dog? (yes or no): ')
+    grilled_onions = input('Would you like grilled onions ($1.00) on your hot dog? (yes or no): ')
+else:
+    hot_dog_price = 0
+
+#if the user wants a chili dog, note the price and ask what toppings the user wants
+if hot_dog_type == 'chili dog':
+    chili_dog_price = 4.50
+    cheese = input('Would you like cheese ($0.50) on your chili dog? (yes or no): ')
+    peppers = input('Would you like peppers ($0.75) on your chili dog? (yes or no): ')
+    grilled_onions = input('Would you like grilled onions ($1.00) on your chili dog? (yes or no): ')
+else:
+    chili_dog_price = 0
+
+#if the user wants cheese, note the price
+if cheese == 'yes':
+    cheese_price = 0.50
+else:
+    cheese_price = 0
+
+#if the user wants peppers, note the price
+if peppers == 'yes':
+    peppers_price = 0.75
+else:
+    peppers_price = 0
+
+#if the user wants grilled onions, note the price
+if grilled_onions == 'yes':
+    grilled_onions_price = 1.00
+else:
+    grilled_onions_price = 0
+
+#calculate the cost by adding up the noted prices
+hot_dog_cost = hot_dog_price + chili_dog_price + cheese_price + peppers_price + grilled_onions_price
+
+#calculate the tax
+tax = 0.07 * hot_dog_cost
+
+#calculate the total cost
+total_cost = hot_dog_cost + tax
+
+#print the calculated information for the user
+print(f'''Hot Dog Cost: ${hot_dog_cost:,.2f}
+Tax: ${tax:,.2f}
+Total Cost: ${total_cost:,.2f}''')
